4162c1c7d1c5853ff524123f1e00452596fcd998
Christian Beer <christian.beer@chbeer.de>
2011-07-20 17:18:56 +0200

    Switched FetchResultDataSource to safe mode.

--- Classes/CBUIFetchResultsDataSource.m
+++ Classes/CBUIFetchResultsDataSource.m
@@ -108,7 +108,7 @@
 
 #pragma mark NSFetchedResultsControllerDelegate
 
-#if 0 // hack because of crappy NSFetchedResultsControllerDelegate
+#if 1 // hack because of crappy NSFetchedResultsControllerDelegate
 
 - (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
     [_tableView reloadData];
