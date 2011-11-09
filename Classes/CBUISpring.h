3cbbf6d9505abb10302e8d8079658ec7a9aa57fc
Christian Beer <christian.beer@chbeer.de>
2011-11-09 07:15:11 +0100

    Started Spring Layout impl.

---
+++ Classes/CBUISpring.h
@@ -0,0 +1,62 @@
+//
+//  CBUISpring.h
+//  CBUIKit
+//
+//  Created by Christian Beer on 08.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class CBUISpringLayoutView;
+
+@interface CBUISpring : NSObject 
+
+@property (nonatomic, readonly) CGFloat minimum;
+@property (nonatomic, readonly) CGFloat maximum;
+@property (nonatomic, readonly) CGFloat preferred;
+
+@property (nonatomic, assign) CGFloat value;
+@property (nonatomic, assign) CGFloat strain;
+
+- (BOOL) isCyclic:(CBUISpringLayoutView*)view;
+
+@end
+
+@interface CBUIAbstractSpring : CBUISpring {
+    CGFloat _size;
+}
+- (void) clear;
+@end
+
+@interface CBUIStaticSpring : CBUIAbstractSpring 
+
+- (id) initWithMinimum:(CGFloat)min preferred:(CGFloat)pref maximum:(CGFloat)max;
+- (id) initWithPreferred:(CGFloat)pref;
+
+@end
+
+@interface CBUINegativeSpring : CBUISpring {
+@private
+    CBUISpring *_spring;
+}
+@end
+
+@interface CBUIScaleSpring : CBUISpring {
+@private
+    CBUISpring *_spring;
+    CGFloat     _factor;
+}
+@end
+
+@interface CBUIWidthSpring : CBUIAbstractSpring {
+@private
+    UIView  *_view;
+}
+@end
+
+@interface CBUIHeightSpring : CBUIAbstractSpring {
+@private
+    UIView  *_view;
+}
+@end
---
+++ Classes/CBUISpringLayoutView.m
@@ -0,0 +1,26 @@
+//
+//  CBUISpringLayoutView.m
+//  CBUIKit
+//
+//  Created by Christian Beer on 08.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CBUISpringLayoutView.h"
+
+@implementation CBUISpringLayoutView
+
+- (id) initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (!self) return nil;
+
+    return self;
+}
+
+-(void) layoutSubviews
+{
+    
+}
+
+@end
