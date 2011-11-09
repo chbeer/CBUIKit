05e01262665ac579c7ece07c7db2b2ed3526693b
Christian Beer <christian.beer@chbeer.de>
2011-11-09 17:04:54 +0100

    Added vertical layout view. Does what it says.

---
+++ Classes/CBUIVerticalLayoutView.h
@@ -0,0 +1,17 @@
+//
+//  CBUIVerticalLayoutView.h
+//  CBUIKit
+//
+//  Created by Christian Beer on 09.11.11.
+//  Copyright (c) 2011 Christian Beer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBUIVerticalLayoutView : UIView
+
+@property (nonatomic, assign) UIEdgeInsets insets;
+
+- (void) setInsetsString:(NSString*)insetsString;
+
+@end
---
+++ Classes/CBUIVerticalLayoutView.m
@@ -0,0 +1,111 @@
+//
+//  CBUIVerticalLayoutView.m
+//  CBUIKit
+//
+//  Created by Christian Beer on 09.11.11.
+//  Copyright (c) 2011 Christian Beer. All rights reserved.
+//
+
+#import "CBUIVerticalLayoutView.h"
+
+@implementation CBUIVerticalLayoutView
+
+@synthesize insets = _insets;
+
+- (id)initWithCoder:(NSCoder *)coder {
+    self = [super initWithCoder:coder];
+    if (!self) return nil;
+    
+    self.insets = UIEdgeInsetsZero;
+    self.autoresizesSubviews = YES;
+    
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (!self) return nil;
+    
+    self.insets = UIEdgeInsetsZero;
+    self.autoresizesSubviews = YES;
+    
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    __block CGFloat widthSum = 0.0;
+
+    CGFloat contentWidth = self.bounds.size.width - self.insets.left - self.insets.right;
+    CGFloat contentHeight = self.bounds.size.height - self.insets.top - self.insets.bottom;
+    
+    NSMutableArray *flexibleViews = [NSMutableArray array];
+    [self.subviews enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        UIView *view = obj;
+        
+        if (view.hidden || view.alpha == 0.0) return;
+        
+        widthSum += view.bounds.size.width;
+        
+        if (view.autoresizingMask & UIViewAutoresizingFlexibleWidth) {
+            [flexibleViews addObject:view];
+        }
+    }];
+    
+    if (widthSum != contentWidth && flexibleViews.count > 0) {
+        CGFloat delta = contentWidth - widthSum;
+        delta = delta / flexibleViews.count;
+        
+        [flexibleViews enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            UIView *view = obj;
+            
+            CGRect bounds = view.bounds;
+            bounds.size.width += delta;
+            view.bounds = bounds;
+        }];
+    }
+    
+    // // Position views // // 
+    
+    __block CGFloat x = self.insets.left;
+    
+    [self.subviews enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        UIView *view = obj;
+        
+        if (view.hidden || view.alpha == 0.0) return;
+        
+        CGRect frame = view.bounds;
+        frame.origin.x = x;
+        frame.origin.y = self.insets.top;
+        if (view.autoresizingMask & UIViewAutoresizingFlexibleHeight) {
+            frame.size.height = contentHeight;
+        }
+        view.frame = frame;
+        
+        x += view.bounds.size.width;
+    }];
+}
+
+#pragma mark - 
+
+- (void)setInsetsString:(NSString *)insetsString
+{
+    NSArray *components = [insetsString componentsSeparatedByString:@","];
+    UIEdgeInsets insets = UIEdgeInsetsZero;
+    if (components.count > 0) {
+        insets.top = [[components objectAtIndex:0] floatValue];
+    }
+    if (components.count > 1) {
+        insets.left = [[components objectAtIndex:1] floatValue];
+    }
+    if (components.count > 2) {
+        insets.bottom = [[components objectAtIndex:2] floatValue];
+    }
+    if (components.count > 3) {
+        insets.right = [[components objectAtIndex:3] floatValue];
+    }
+    self.insets = insets;
+}
+
+@end
