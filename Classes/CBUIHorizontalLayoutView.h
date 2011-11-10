f6a1a8ad0498c9883d13f12620cb7eb9a4d67980
Christian Beer <christian.beer@chbeer.de>
2011-11-10 17:41:29 +0100

    Added horizontal layout

---
+++ Classes/CBUIHorizontalLayoutView.h
@@ -0,0 +1,17 @@
+//
+//  CBUIHorizontalLayoutView.h
+//  CBUIKit
+//
+//  Created by Christian Beer on 09.11.11.
+//  Copyright (c) 2011 Christian Beer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBUIHorizontalLayoutView : UIView
+
+@property (nonatomic, assign) UIEdgeInsets insets;
+
+- (void) setInsetsString:(NSString*)insetsString;
+
+@end
